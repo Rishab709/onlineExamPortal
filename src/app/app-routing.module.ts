0c070fc61c92d18ef6b6f252fa054048269176ff
Rishab709 <rishabsaha1995@outlook.com>
2020-12-27 03:41:43 +0530

    register form design started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CandidateDetailsComponent } from './candidate-details/candidate-details.component';
+import { ExamRulesComponent } from './exam-rules/exam-rules.component';
+import { ExamDashboardComponent } from './exam-dashboard/exam-dashboard.component';
+import { ExamSummaryComponent } from './exam-summary/exam-summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:CandidateDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
